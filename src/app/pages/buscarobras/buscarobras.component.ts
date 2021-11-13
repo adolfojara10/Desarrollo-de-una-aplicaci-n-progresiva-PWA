cd7f490227c03dd12b6bd8c8b3b093bb249f7da1
BryamBepz <bryamparra2709@gmail.com>
2021-11-13 16:54:15 -0500

    HTML de la pagina de busque de obras

--- src/app/pages/buscarobras/buscarobras.component.ts
+++ src/app/pages/buscarobras/buscarobras.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-buscarobras',
@@ -7,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarobrasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    
   }
 
+  abrir(){
+    this.router.navigate(['principal']);
+  }
 }
